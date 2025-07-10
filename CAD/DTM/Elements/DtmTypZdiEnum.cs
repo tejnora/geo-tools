d8d2f2313c7c71a2750cba6a370ae8093b794244
David <david.tejnora@gmail.com>
2025-07-10 09:19:02 +0200

    Dtm

---
+++ CAD/DTM/Elements/DtmTypZdiEnum.cs
@@ -0,0 +1,11 @@
+﻿namespace CAD.DTM.Elements
+{
+    enum DtmTypZdiEnum
+    {
+        VolneStojici=1,
+        Operna=2,
+        Zarubni=3,
+        MestskeHradby=4,
+        Nezjisteno=99
+    }
+}
---
+++ CAD/DTM/Elements/Linie/DtmHraniceZdiElement.cs
@@ -0,0 +1,48 @@
+﻿using CAD.DTM.Configuration;
+using System.Collections.Generic;
+using System;
+using CAD.DTM.Gui;
+using System.Xml;
+
+namespace CAD.DTM.Elements
+{
+    class DtmHraniceZdiElement
+        : DtmLinieElementBase
+    {
+
+        public DtmTypZdiEnum TypZdi { get; set; }
+        public override void ExportAttributesToDtm(IDtmExporter exporter)
+        {
+            ExportSpolecneAtributyObjektuZPS(exporter);
+            exporter.AddElement("atr", "TypZdi", (int)TypZdi);
+        }
+        public override void ImportDtmAttributes(XmlElement xmlElement)
+        {
+            base.ImportDtmAttributes(xmlElement);
+            foreach (XmlElement x in xmlElement)
+            {
+                switch (x.LocalName)
+                {
+                    case "TypZdi":
+                        TypZdi = (DtmTypZdiEnum)int.Parse(x.InnerText);
+                        break;
+                }
+            }
+        }
+        public override void Init(DtmElementOption dtmElementOption)
+        {
+            base.Init(dtmElementOption);
+            TypZdi = DtmTypZdiEnum.Nezjisteno;
+            SpolecneAtributyZPS = new DtmSpolecneAtributyZPS();
+        }
+        public override IEnumerable<string> Settings => Enum.GetNames(typeof(DtmTypStavbyEnum));
+        public override void SelectedSetting(string value)
+        {
+            TypZdi = (DtmTypZdiEnum)Enum.Parse(typeof(DtmTypZdiEnum), value);
+        }
+        public override string GetInfoAsString()
+        {
+            return $"Type stavby: {TypZdi}";
+        }
+    }
+}
